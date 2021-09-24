0c5e9e88a89dedf647afb5e16901de3606b87e2e
ohadleib <ohadleib@gmail.com>
2021-09-25 00:23:32 +0300

    covid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Graph from "./pages/Graph";
-import Nav from "./Nav";
-import Footer from "./Footer";
-// import Symptom from "./pages/Symptom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Nav />
+      <div> 
         <Switch>
           <Route exact path="/covid-19" component={Home} />
-          {/* <Route path="/symptom" component={Symptom} />
-          <Route path="/graph" component={Graph} />
-          <Route path="/about" component={About} /> */}
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
