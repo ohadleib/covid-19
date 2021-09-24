fbc09921bfa3d0f444fa8bd52ed712830462e583
ohadleib <ohadleib@gmail.com>
2021-09-24 22:02:34 +0300

    Updates

---
+++ precache-manifest.1a91a05222700b7c3d681e3fdfe4a486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b139c032657816e5d733e42dd335f526",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "b931e56713c7d00b79bc",
+    "url": "/covid-19/static/css/2.0c73dda4.chunk.css"
+  },
+  {
+    "revision": "980d58abc57d528663c5",
+    "url": "/covid-19/static/css/main.8ed926c7.chunk.css"
+  },
+  {
+    "revision": "b931e56713c7d00b79bc",
+    "url": "/covid-19/static/js/2.2a888414.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19/static/js/2.2a888414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980d58abc57d528663c5",
+    "url": "/covid-19/static/js/main.a8bfa66b.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
